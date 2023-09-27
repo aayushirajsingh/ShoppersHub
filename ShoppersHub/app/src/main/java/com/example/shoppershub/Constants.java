92c2fb7b8e329a9e5f80b9f9617213da6f7836d2
Aayushi <86465113+aayushirajsingh@users.noreply.github.com>
2023-09-27 12:22:11 +0530

    Update Constants.java

--- ShoppersHub/app/src/main/java/com/example/shoppershub/Constants.java
+++ ShoppersHub/app/src/main/java/com/example/shoppershub/Constants.java
@@ -3,6 +3,6 @@ package com.example.shoppershub;
 public class Constants {
 
     // Change the IP according to computer's IP.
-    public static final String IP = "192.168.1.__";
+    public static final String IP = "";
     public static final String BASE_URL_IP = "http://"+IP+"/shoppershub_backend/";
 }
